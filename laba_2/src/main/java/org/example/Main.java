db50efb489d97cfb4770f20d2236a2b5927a8607
Sonya <sonya.galtsovaa@gmail.com>
2025-04-02 12:35:54 +0300

    basic calculator

--- laba_2/src/main/java/org/example/Main.java
+++ laba_2/src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
+// Main.java
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter simple expression (a + b): ");
+        double a = scanner.nextDouble();
+        String op = scanner.next();
+        double b = scanner.nextDouble();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double result = 0;
+        switch(op) {
+            case "+": result = a + b; break;
+            case "-": result = a - b; break;
+            case "*": result = a * b; break;
+            case "/": result = a / b; break;
         }
+
+        System.out.println("Result: " + result);
+        scanner.close();
     }
 }
